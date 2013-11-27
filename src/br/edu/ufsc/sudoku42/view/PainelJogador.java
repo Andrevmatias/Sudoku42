592afd27b7d7160911c95710e459cb677a623d13
Jhonata da Rocha <jhonata182@gmail.com>
2013-11-27 16:25:47 -0200

    projeto de timer engatinhando

--- src/br/edu/ufsc/sudoku42/view/PainelJogador.java
+++ src/br/edu/ufsc/sudoku42/view/PainelJogador.java
@@ -1,6 +1,8 @@
 package br.edu.ufsc.sudoku42.view;
 
 import java.awt.Dimension;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -11,6 +13,11 @@ public class PainelJogador extends JPanel{
 	protected String nome;
 	protected JLabel tempo;
 	protected JLabel pontuacao;
+	
+	int minutos = 10;
+	int segundos = 0;
+
+	Timer timer;
 	
 	public PainelJogador(String nome) {
 		this.nome = nome;
@@ -26,6 +33,43 @@ public class PainelJogador extends JPanel{
 		this.add(tempo);
 		this.add(pontuacao);
 		this.setPreferredSize(new Dimension(100,40));
+		this.bla();
+		
 	}
 	
+	public void bla(){
+		int interval;
+		Timer timer;
+		
+	    int delay = 1000;
+	    int period = 1000;
+	    timer = new Timer();
+
+	    timer.scheduleAtFixedRate(new TimerTask() {
+
+	        public void run() {
+	            setInterval();
+
+	        }
+	    }, delay, period);
+	}
+	
+	
+
+
+	private void setInterval() {
+
+		if(segundos <= 0){
+			minutos--;
+			segundos = 59;
+			
+			
+		} else {
+			segundos --;
+			
+		}
+		
+		tempo.setText(Integer.toString(minutos) + ":" + Integer.toString(segundos));
+		 
+	}
 }
