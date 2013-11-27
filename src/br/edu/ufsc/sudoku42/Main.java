8949009b5fc0183910c39d759210dc9f0cea4fb0
Jhonata da Rocha <jhonata182@gmail.com>
2013-11-27 16:59:12 -0200

    demoção de espaços no código

--- src/br/edu/ufsc/sudoku42/Main.java
+++ src/br/edu/ufsc/sudoku42/Main.java
@@ -5,9 +5,8 @@ import br.edu.ufsc.sudoku42.view.InterfaceJogador;
 public class Main {
 
 	public static void main(String[] args) {
-		InterfaceJogador janela = new InterfaceJogador(/*TODO: Injetar dependências*/);
+		InterfaceJogador janela = new InterfaceJogador(/*TODO: Injetar dependï¿½ncias*/);
 		janela.setVisible(true);
-		
 	}
 
 }
