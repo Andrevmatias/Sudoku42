34937c975e698798225afeba75a1b57258789e71
Jhonata da Rocha <jhonata11@hotmail.com>
2013-11-27 16:52:27 -0200

    melhorias na interface do tempo

--- src/br/edu/ufsc/sudoku42/view/PainelJogador.java
+++ src/br/edu/ufsc/sudoku42/view/PainelJogador.java
@@ -10,66 +10,75 @@ import javax.swing.JPanel;
 
 public class PainelJogador extends JPanel{
 	
-	protected String nome;
-	protected JLabel tempo;
-	protected JLabel pontuacao;
-	
-	int minutos = 10;
-	int segundos = 0;
+	protected static final String TEMPLATE_TIME_1 =    ":";
+    protected static final String TEMPLATE_TIME_2 =    ":0";
+   
+    protected String nome;
+    protected JLabel tempo;
+    protected JLabel pontuacao;
+   
+    protected int minutos = 10;
+    protected int segundos = 0;
 
-	Timer timer;
-	
-	public PainelJogador(String nome) {
-		this.nome = nome;
-		initialize();
+    protected Timer timer;
+   
+    public PainelJogador(String nome) {
+        this.nome = nome;
+        initialize();
 
-	}
+    }
 
-	public void initialize() {
-		this.setBorder(BorderFactory.createTitledBorder(this.nome));
-		
-		this.tempo = new JLabel("00:00");
-		this.pontuacao = new JLabel("99");
-		this.add(tempo);
-		this.add(pontuacao);
-		this.setPreferredSize(new Dimension(100,40));
-		this.bla();
-		
-	}
-	
-	public void bla(){
-		int interval;
-		Timer timer;
-		
-	    int delay = 1000;
-	    int period = 1000;
-	    timer = new Timer();
+    public void initialize() {
+        this.setBorder(BorderFactory.createTitledBorder(this.nome));
+       
+        this.tempo = new JLabel("00:00");
+        this.pontuacao = new JLabel("99");
+        this.add(tempo);
+        this.add(pontuacao);
+        this.setPreferredSize(new Dimension(100,40));
+        this.bla();
+       
+    }
+   
+    public void bla(){
+        int interval;
+        Timer timer;
+       
+        int delay = 1000;
+        int period = 1000;
+        timer = new Timer();
 
-	    timer.scheduleAtFixedRate(new TimerTask() {
+        timer.scheduleAtFixedRate(new TimerTask() {
 
-	        public void run() {
-	            setInterval();
+            public void run() {
+                setInterval();
 
-	        }
-	    }, delay, period);
-	}
-	
-	
+            }
+        }, delay, period);
+    }
+   
+   
 
 
-	private void setInterval() {
+    private void setInterval() {
+        StringBuilder text = new StringBuilder("0");
 
-		if(segundos <= 0){
-			minutos--;
-			segundos = 59;
-			
-			
-		} else {
-			segundos --;
-			
-		}
-		
-		tempo.setText(Integer.toString(minutos) + ":" + Integer.toString(segundos));
-		 
-	}
+        if(segundos <= 0){
+            minutos--;
+            segundos = 59;
+        } else {
+            segundos --;
+        }
+       
+        text.append(minutos);
+        if(segundos< 10){
+            text.append(TEMPLATE_TIME_2);
+        } else {
+            text.append(TEMPLATE_TIME_1);
+        }
+        text.append(segundos);
+       
+        tempo.setText(text.toString());
+         
+    }
 }
