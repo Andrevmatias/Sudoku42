851a537266e5ead9e3fe0045f3028c422cfd05ce
Jhonata da Rocha <jhonata182@gmail.com>
2013-12-06 16:36:11 -0200

    adicionados botões de desistir e desconectar à barra de tarefas, apenas habilitando quando for conectado ou com partida iniciada

--- src/br/edu/ufsc/sudoku42/view/BarraDeTarefas.java
+++ src/br/edu/ufsc/sudoku42/view/BarraDeTarefas.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JSeparator;
 
 import br.edu.ufsc.sudoku42.network.NetworkException;
 
@@ -14,8 +15,11 @@ public class BarraDeTarefas extends JMenuBar {
 	private static final long serialVersionUID = -2313774782441238222L;
 	
 	protected JMenu menuOpcoes;
+	
 	protected JMenuItem itemConectar;
 	protected JMenuItem itemIniciarPartida;
+	protected JMenuItem itemDesistir;
+	protected JMenuItem itemDesconectar;
 	
 	protected InterfaceJogador interfaceJogador;
 	
@@ -23,9 +27,24 @@ public class BarraDeTarefas extends JMenuBar {
 	public BarraDeTarefas(InterfaceJogador interfaceJogador) {
 		this.interfaceJogador = interfaceJogador;
 		
+		this.instanciarAtributos();
+		this.adicionarActionListener();
+		this.setValorDefaultAtributos();
+		this.adicionarComponentes();
+	}
+	
+	
+	public void instanciarAtributos(){
 		this.menuOpcoes = new JMenu("Op��es");
 		
 		this.itemConectar = new JMenuItem("Conectar");
+		this.itemIniciarPartida = new JMenuItem("Iniciar Partida");
+		this.itemDesistir = new JMenuItem("Desistir");
+		this.itemDesconectar = new JMenuItem("Desconectar");
+		
+	}
+	
+	public void adicionarActionListener(){
 		this.itemConectar.addActionListener(new ActionListener() {
 			
 			@Override
@@ -34,18 +53,19 @@ public class BarraDeTarefas extends JMenuBar {
 				String nome = getInterfaceJogador().requisitarNome();
 				getInterfaceJogador().conectar(nome);
 				itemIniciarPartida.setEnabled(true);
+				itemDesconectar.setEnabled(true);
 				
 			}
 		});
 		
 		
-		this.itemIniciarPartida = new JMenuItem("Iniciar Partida");
 		this.itemIniciarPartida.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
 					getInterfaceJogador().iniciarPartida();
+					itemDesistir.setEnabled(true);
 				} catch (NetworkException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -54,13 +74,47 @@ public class BarraDeTarefas extends JMenuBar {
 				
 			}
 		});
-		this.itemIniciarPartida.setEnabled(false);
 		
+		this.itemDesconectar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO: desistir
+				
+			}
+		});
+		
+		this.itemDesistir.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO: desistir
+				
+			}
+		});
+		
+		
+	}
+	
+	public void adicionarComponentes(){
 		this.menuOpcoes.add(this.itemConectar);
 		this.menuOpcoes.add(this.itemIniciarPartida);
+		
+		this.menuOpcoes.add(new JSeparator());
+		
+		this.menuOpcoes.add(this.itemDesistir);
+		this.menuOpcoes.add(this.itemDesconectar);
+		
 		this.add(menuOpcoes);
 		
 	}
+	
+	public void setValorDefaultAtributos(){
+		this.itemIniciarPartida.setEnabled(false);
+		this.itemDesistir.setEnabled(false);
+		this.itemDesconectar.setEnabled(false);
+	}
+	
 	
 	public InterfaceJogador getInterfaceJogador(){
 		return this.interfaceJogador;
